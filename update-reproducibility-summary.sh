1534212c527c3b611d5fb64297f826e42230af98
Hervé Boutemy <hboutemy@apache.org>
2020-04-04 18:34:46 +0200

    added latest release reproducibility

--- update-reproducibility-summary.sh
+++ update-reproducibility-summary.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-cat <(echo "| groupId | artifactId  | HEAD version: Reproducibility |"
-echo "| ------- | ------------------ | ----------------- |"
+cat <(echo "| groupId | artifactId: buildspec  | latest release: Reproducibility | HEAD version: Reproducibility |"
+echo "| ------- | ------------------------- | ----------------- | ----------------- |"
 
 count=0
 countHEADOk=0
 sumHEADOk=0
 sumHEADKo=0
+countLatestOk=0
+sumLatestOk=0
+sumLatestKo=0
 prevDist=""
 for buildspec in `find maven -name *.buildspec -print | sort`
 do
@@ -24,8 +27,10 @@ do
   buildinfo="`dirname ${buildspec}`/`basename ${buildinfo}`"
 
   echo -n "| [`echo ${groupId} | sed s/org.apache.maven/m/`](https://repo.maven.apache.org/maven2/${groupDir}) "
-  echo -n "| [${artifactId}](https://repo.maven.apache.org/maven2/${groupDir}/${artifactId}) "
+  echo -n "| [${artifactId}](https://repo.maven.apache.org/maven2/${groupDir}/${artifactId}): "
   echo -n "[spec](https://github.com/jvm-repo-rebuild/reproducible-maven-HEAD/tree/master/${buildspec}) "
+
+  echo -n "| x.y.z "
 
   . ${buildinfo}.compare
   echo -n "| [${version}](https://github.com/jvm-repo-rebuild/reproducible-maven-HEAD/tree/master/"`ls ${buildinfo}`"): "
@@ -37,12 +42,27 @@ do
   sumHEADOk=$(($sumHEADOk + ${ok}))
   sumHEADKo=$(($sumHEADKo + ${ko}))
 done
-echo -n "| | **${count}** | **${countHEADOk} :heavy_check_mark: + $((${count}-${countHEADOk})) :warning:: "
-echo -n "$((${countHEADOk}*100/${count}))% :heavy_check_mark: "
-echo "${sumHEADOk} :heavy_check_mark: ${sumHEADKo} :warning:** |"
-echo "Total: ${count} sub-projects
 
-**HEAD Reproducibility: ${countHEADOk} :heavy_check_mark: + $((${count}-${countHEADOk})) :warning:
+# temporary hard coded latest figures
+countLatestOk=6
+sumLatestOk=53
+sumLatestKo=529
+
+echo -n "| | **${count}** "
+
+echo -n "| **${countLatestOk} :heavy_check_mark: + $((${count}-${countLatestOk})) :warning: = "
+echo -n "$((${countLatestOk}*100/${count}))% :heavy_check_mark:: "
+echo -n "${sumLatestOk} :heavy_check_mark: ${sumLatestKo} :warning:** "
+
+echo -n "| **${countHEADOk} :heavy_check_mark: + $((${count}-${countHEADOk})) :warning: = "
+echo -n "$((${countHEADOk}*100/${count}))% :heavy_check_mark:: "
+echo "${sumHEADOk} :heavy_check_mark: ${sumHEADKo} :warning:** |"
+echo "Total: Apache Maven consists in ${count} sub-projects:
+
+- **Latest release Reproducibility: ${countLatestOk} :heavy_check_mark: + $((${count}-${countLatestOk})) :warning:
+   = $((${countLatestOk}*100/${count}))% :heavy_check_mark:**
+
+- **HEAD Reproducibility: ${countHEADOk} :heavy_check_mark: + $((${count}-${countHEADOk})) :warning:
    = $((${countHEADOk}*100/${count}))% :heavy_check_mark:**
    " > summary.md
 ) > summary-table.md
