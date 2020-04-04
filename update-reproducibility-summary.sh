8a7b5496110a2a7a2f297e291631901cb405e402
Hervé Boutemy <hboutemy@apache.org>
2020-04-04 18:18:23 +0200

    clarify HEAD reproducibility

--- update-reproducibility-summary.sh
+++ update-reproducibility-summary.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cat <(echo "| groupId | artifactId | build | HEAD version | Reproducibility |"
-echo "| ------- | ------------------ | ----- | ------------ | ----- |"
+cat <(echo "| groupId | artifactId  | HEAD version: Reproducibility |"
+echo "| ------- | ------------------ | ----------------- |"
 
 count=0
-countOk=0
-sumOk=0
-sumKo=0
+countHEADOk=0
+sumHEADOk=0
+sumHEADKo=0
 prevDist=""
 for buildspec in `find maven -name *.buildspec -print | sort`
 do
@@ -25,29 +25,25 @@ do
 
   echo -n "| [`echo ${groupId} | sed s/org.apache.maven/m/`](https://repo.maven.apache.org/maven2/${groupDir}) "
   echo -n "| [${artifactId}](https://repo.maven.apache.org/maven2/${groupDir}/${artifactId}) "
-  echo -n "| [spec](https://github.com/jvm-repo-rebuild/reproducible-maven-HEAD/tree/master/${buildspec}) "
-  [ -f ${buildinfo} ] && echo -n "/ [info](https://github.com/jvm-repo-rebuild/reproducible-maven-HEAD/tree/master/"`ls ${buildinfo}`") "
+  echo -n "[spec](https://github.com/jvm-repo-rebuild/reproducible-maven-HEAD/tree/master/${buildspec}) "
 
   . ${buildinfo}.compare
-  echo -n "| ${version} "
-  if [ $? -eq 0 ]; then
-    echo -n "| "
-    [ ${ok} -gt 0 ] && echo -n "${ok} :heavy_check_mark: "
-    [ ${ko} -gt 0 ] && echo -n " ${ko} [:warning:](https://github.com/jvm-repo-rebuild/reproducible-maven-HEAD/tree/master/`ls ${buildinfo}.compare`) "
-  else
-    echo -n "| :x: "
-  fi
+  echo -n "| [${version}](https://github.com/jvm-repo-rebuild/reproducible-maven-HEAD/tree/master/"`ls ${buildinfo}`"): "
+  [ ${ok} -gt 0 ] && echo -n "${ok} :heavy_check_mark: "
+  [ ${ko} -gt 0 ] && echo -n " ${ko} [:warning:](https://github.com/jvm-repo-rebuild/reproducible-maven-HEAD/tree/master/`ls ${buildinfo}.compare`) "
   echo "|"
 
-  [ ${ko} -eq 0 ] && countOk=$(($countOk + 1))
-  sumOk=$(($sumOk + ${ok}))
-  sumKo=$(($sumKo + ${ko}))
+  [ ${ko} -eq 0 ] && countHEADOk=$(($countHEADOk + 1))
+  sumHEADOk=$(($sumHEADOk + ${ok}))
+  sumHEADKo=$(($sumHEADKo + ${ko}))
 done
-echo -n "|  | **${count} = ${countOk} :heavy_check_mark: + $((${count}-${countOk})) :warning:** "
-echo -n "| **$((${countOk}*100/${count}))% :heavy_check_mark:** "
-echo "|  | **${sumOk} :heavy_check_mark: ${sumKo} :warning:** |"
-echo "**Total: ${count} sub-projects = ${countOk} :heavy_check_mark: + $((${count}-${countOk})) :warning:
-   = $((${countOk}*100/${count}))% :heavy_check_mark:**
+echo -n "| | **${count}** | **${countHEADOk} :heavy_check_mark: + $((${count}-${countHEADOk})) :warning:: "
+echo -n "$((${countHEADOk}*100/${count}))% :heavy_check_mark: "
+echo "${sumHEADOk} :heavy_check_mark: ${sumHEADKo} :warning:** |"
+echo "Total: ${count} sub-projects
+
+**HEAD Reproducibility: ${countHEADOk} :heavy_check_mark: + $((${count}-${countHEADOk})) :warning:
+   = $((${countHEADOk}*100/${count}))% :heavy_check_mark:**
    " > summary.md
 ) > summary-table.md
 
